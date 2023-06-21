d7afd7d81f7bbe155c665b3683c289606e296e7c
Barry Hanna <barryahanna@gmail.com>
2023-06-21 18:05:26 +0100

    Exercise 2.4 and 2.5

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,11 @@
 import Part from './Part';
+import { v4 as uuid } from 'uuid';
 
 const Content = ({ parts }) => {
 	return (
 		<>
 			{parts.map((part) => (
-				<Part part={part} />
+				<Part key={uuid()} part={part} />
 			))}
 		</>
 	);
